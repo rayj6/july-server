4437fe9707f499ac07d86db8948aa5502fd442bd
RayJ5 <rayjworka@gmail.com>
2024-01-06 02:20:40 +0700

    Updated

--- functions/database/database.config.js
+++ functions/database/database.config.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    // host: "localhost",
-    // user: "root",
-    // password: "",
-    // database: "july",
-    // port: 3307,
-    host: "0.tcp.ap.ngrok.io",
+    host: "localhost",
     user: "root",
     password: "",
     database: "july",
-    port: 11664,
+    port: 3307,
+    // host: "0.tcp.ap.ngrok.io",
+    // user: "root",
+    // password: "",
+    // database: "july",
+    // port: 11664,
 });
 
 module.exports = connection;
